ac03167b7901ce0df01017c2dff27013daaae07b
lbigearde <lbigearde>
2008-03-28 18:02:44 +0000

    test CVS rights

--- plugins/org.eclipse.gymnast.generators.parser.antlr/src/org/eclipse/gymnast/generators/parser/antlr/Activator.java
+++ plugins/org.eclipse.gymnast.generators.parser.antlr/src/org/eclipse/gymnast/generators/parser/antlr/Activator.java
@@ -17,7 +17,7 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends Plugin { 
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.gymnast.generators.parser.antlr";
